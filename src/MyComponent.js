b7ed3bdba63188ef24e91ff6a296b3f1026c66f9
TristanSn <tristan108.sarrazin@gmail.com>
2021-11-23 10:25:54 +0100

    test taupe

--- src/MyComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class MyComponent extends React.Component {
-    render() {
-        return<h1>Test composant</h1>
-    }
-} //andjzjdzidizodozodzo
-
-export default MyComponent;
---
+++ src/taupe.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+/*class Taupe extends React.Component {
+    render() {
+        return<h1>Test composant</h1>
+    }
+}*/
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Taupe extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'm';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Taupe;
